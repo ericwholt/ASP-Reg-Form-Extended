ff875de52d9fa123a1ae7cbc69e66bb279ca3b89
Eric Holt <fatmoti@gmail.com>
2019-05-22 12:58:41 -0400

    Initial Commit

---
+++ ASP-Reg-Form/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP_Reg_Form.Models;
+
+namespace ASP_Reg_Form.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult Summary(User user)
+        {
+            ViewBag.FullName = user.FullName;
+            //ViewBag.Email = user.Email;
+            //ViewBag.UserName = user.UserName;
+            //ViewBag.Password = user.Password;
+            return View();
+        }
+    }
+}
---
+++ ASP-Reg-Form/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP_Reg_Form.MvcApplication" Language="C#" %>
